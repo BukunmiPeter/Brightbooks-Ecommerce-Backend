513f6164ccdaf1beacdaf5030a49bb044f4aa4f9
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2022-04-26 14:16:04 +0100

    update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const expressValidator = require("express-validator");
+const path = require("path");
 //import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -13,6 +14,7 @@ const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const braintreeRoutes = require("./routes/braintree");
 const orderRoutes = require("./routes/order");
+const res = require("express/lib/response");
 // app
 const app = express();
 
@@ -35,6 +37,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
+// app.use(express.static(path.join(__dirname, "BUKIEWEARSBACKEND", "build")));
 
 //routes middleware
 
@@ -46,6 +49,12 @@ app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 8000;
+
+// app.get("*", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "BUKIEWEARSBACKEND", "build", "index.html")
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port${port} `);
